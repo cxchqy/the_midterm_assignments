0870fc3c85d6d4969458dc2bf39619e86971c6de
cxc <cxcodd@gmail.com>
2018-08-14 21:36:02 +0800

    Add the average score of each subject and total average score,Corrected the order

--- the_mid1.py
+++ the_mid1.py
@@ -12,11 +12,9 @@ with open('report.txt', 'r', encoding='utf-8') as f:
         a = l.split()
         data.append(a)
 
-
 # ------计算总分-------
 l1 = len(data)
 l2 = len(data[0])
-print(l2)
 for number in range(1, l1):
     d1 = data[number]
     total_scores = 0
@@ -31,32 +29,33 @@ for number in range(1, l1):
     d1.append(str(total_scores))
     d1.append(str(round(ave_scores, 1)))
 
-# -----添加标题行-------
-data[0].insert(0, '名次')
-data[0].append('总分')
-data[0].append('平均分')
-
-l2 += 2
-# ------汇总每课平均分--------
-for order in range(1, l2):
-    ave_all = 0
-    for number in range(1, l1):
-        ave_all += float(data[number][order])
-        if float(data[number][order]) < 60:
-            data[number][order] = '不及格'
-    if l1 > 1:
-        ave = ave_all / (l1-1)
-    else:
-        ave = 0
-    header.append(str(round(ave, 1)))
-data.insert(1, header)
-
 # ------排序---------
 data.sort(key=lambda x: x[-1], reverse=True)
 
 # -----添加排名------
-for number in range(1, l1+1):
-    data[number].insert(0, str(number-1))
+for number in range(0, l1):
+    data[number].insert(0, str(number))
+
+# ------汇总每课平均分--------
+ave = 0
+for width in range(2, l2+3):
+    ave_all = 0
+    for length in range(1, l1):
+        ave_all += float(data[length][width])
+        if float(data[length][width]) < 60:
+            data[length][width] = '不及格'
+        if l1 > 1:
+            ave = ave_all/(l1-1)
+        else:
+            ave = 0
+    header.append(str(round(ave, 1)))
+header.insert(0, '0')
+data.insert(1, header)
+
+# -----添加标题行-------
+data[0][0] = '名次'
+data[0].append('总分')
+data[0].append('平均分')
 
 # ------输出到文件-------
 for i in data:
